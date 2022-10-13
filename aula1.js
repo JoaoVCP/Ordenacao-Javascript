5975216a0bbbcae5b7d85e575e49f53c3ec1864e
JoaoVCP <jvcallera@gmail.com>
2022-10-13 16:20:09 -0300

    Transformando em objeto e trabalhando ordenacao

--- aula1.js
+++ aula1.js
@@ -1,11 +1,12 @@
-const precoLivros = [25, 15, 30, 50, 45, 20];
+// const precoLivros = [25, 15, 30, 50, 45, 20];
+const livros = require('./listaLivros');
 
 let maisBarato = 0;
 
-for(let atual = 0; atual < precoLivros.length; atual++){
-    if (precoLivros[atual] < precoLivros[maisBarato]){
+for(let atual = 0; atual < livros.length; atual++){
+    if (livros[atual] < livros[maisBarato]){
         maisBarato = atual;
     }
 }
 
-console.log(`O livro mais barato custa ${precoLivros[maisBarato]}`);
+console.log(`O livro mais barato custa ${livros[maisBarato].preco} e o titulo e ${livros[maisBarato].titulo}`);
